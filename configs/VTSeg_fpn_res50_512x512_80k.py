91a9d563df89b1b47388ca6b0e982927bef8c821
liualice123 <tysylqq2015@163.com>
2024-06-19 11:19:20 +0800

    Update VTSeg_fpn_res50_512x512_80k.py

--- configs/VTSeg_fpn_res50_512x512_80k.py
+++ configs/VTSeg_fpn_res50_512x512_80k.py
@@ -44,7 +44,7 @@ model = dict(
         loss_decode=[dict(
             type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1.0),
             dict(
-            type='DiceLoss', use_sigmoid=False, loss_weight=0.1)
+            type='ContrastLoss', use_sigmoid=False, loss_weight=0.01)
         ]
         ),
 )
